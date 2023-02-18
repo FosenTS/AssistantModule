5b7e6b029b37b536f480478ef6aa8a5202cdfc48
FosenTS <juster050@yandex.com>
2023-02-18 12:39:43 +0300

    Fix speak function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const { text } = require("express");
+
 function listen() {
     let inputArea = document.getElementById('input-area');
     let outputArea = document.getElementById('output-area');
@@ -6,19 +8,24 @@ function listen() {
     recognition.lang = "ru_RU";
     recognition.start();
 
-    const textEl = document.getElementById()
     recognition.onresult = function(event) {
     let transcript = event.results[0][0].transcript;
 	  if (transcript.includes("Привет")) {
 		  outputArea.innerHTML = "Привет, пользователь!";
-      const textEl = document.getElementById('output-area');
 	  }
       inputArea.innerHTML = event.results[0][0].transcript;
     }
-    window.speechSynthesis.cancel();
-    const text = textEl.ariaValueMax;
-    const utterance = new SpeechSynthesisUtterance(text);
-    window.speechSynthesis.speak(utterance);
+    speak();
+}
+
+let textEl = document.getElementById('output-area');
+
+function speak() {
+  window.speechSynthesis.cancel();
+
+  const text = textEl.value;
+  const utterance = new SpeechSynthesisUtterance(text);
+  window.speechSynthesis.speak(utterance);
 }
 
 
