6095a752b114e215639ab751503d4070670f8fef
FosenTS <76241314+FosenTS@users.noreply.github.com>
2023-02-18 14:56:10 +0300

    add hand input function

--- script.js
+++ script.js
@@ -11,10 +11,30 @@ function listen() {
     recognition.onresult = function(event) {
     let transcript = event.results[0][0].transcript;
 	  if (transcript.includes("Привет")) {
-		  outputArea.innerHTML = "Привет, пользователь!";
+      let text = "Привет, пользователь!";
+		  outputArea.innerHTML = text;
+      speechSynthesis.speak(new SpeechSynthesisUtterance(text));
 	  }
+    else {
+      let text = "В данный момент ассистент не способен ответить на ваш вопрос. Мы перенаправим ваш вопрос в банк вопросов, для формирования ответа";
+      outputArea.innerHTML = text;
+      speechSynthesis.speak(new SpeechSynthesisUtterance(text));
+    }
       inputArea.innerHTML = event.results[0][0].transcript;
     }
 }
 
-
+function handInput() {
+  let transcript = document.getElementById('LabelText');
+  if (transcript.includes("Привет")) {
+    let text = "Привет, пользователь!";
+    outputArea.innerHTML = text;
+    speechSynthesis.speak(new SpeechSynthesisUtterance(text));
+  }
+  else {
+    let text = "В данный момент ассистент не способен ответить на ваш вопрос. Мы перенаправим ваш вопрос в банк вопросов, для формирования ответа";
+    outputArea.innerHTML = text;
+    speechSynthesis.speak(new SpeechSynthesisUtterance(text));
+  }
+    inputArea.innerHTML = event.results[0][0].transcript;
+}
