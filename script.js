0de387c4374ee4e16981e2b6741614064c08ebfa
FosenTS <76241314+FosenTS@users.noreply.github.com>
2023-02-18 16:25:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ function listen() {
 		  outputArea.innerHTML = text;
       speechSynthesis.speak(new SpeechSynthesisUtterance(text));
 	  }
-    else if (transcript.includes("обратная связь по гранту")){
-      let text = "Добрый день,\nДля запроса обратной связи необходимо зайти в информацию о заявке, путем нажатия на <<ЗАЯВКА НА ГРАНТОВЫЙ КОНКУРС ПОДАНА>> в разделе мои заявки.\n С Уважением,\nТехническая поддержка АИС Молодежь России"
+    else if (transcript.includes("обратная связь по гранту") || transcript.includes("обратная связь по Гранту")){
+      let text = "Добрый день, для запроса обратной связи необходимо зайти в информацию о заявке, путем нажатия на <<ЗАЯВКА НА ГРАНТОВЫЙ КОНКУРС ПОДАНА>> в разделе мои заявки. С Уважением, Техническая поддержка АИС Молодежь России"
       outputArea.innerHTML = text;
       speechSynthesis.speak(new SpeechSynthesisUtterance(text));
     }
