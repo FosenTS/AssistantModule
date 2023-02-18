0b6759f04fa43350c34f56f01e92b8180e1ace77
FosenTS <juster050@yandex.com>
2023-02-18 12:27:22 +0300

    added to text-to-speech

--- script.js
+++ script.js
@@ -5,13 +5,21 @@ function listen() {
     var recognition = new webkitSpeechRecognition();
     recognition.lang = "ru_RU";
     recognition.start();
-  
+
+    const textEl = document.getElementById()
     recognition.onresult = function(event) {
     let transcript = event.results[0][0].transcript;
 	  if (transcript.includes("Привет")) {
 		  outputArea.innerHTML = "Привет, пользователь!";
+      const textEl = document.getElementById('output-area');
 	  }
       inputArea.innerHTML = event.results[0][0].transcript;
     }
+    window.speechSynthesis.cancel();
+    const text = textEl.ariaValueMax;
+    const utterance = new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(utterance);
 }
+
+
 
